d2469c61de6f262b180408ef256d76ae8013d5d6
femarko <fenstermark7@gmail.com>
2025-05-05 02:34:39 +0200

    feat: figures, protocols modified, main added.

--- src/figures.py
+++ src/figures.py
@@ -1,17 +1,67 @@
-from math import pi
+import math
+
+from typing import Union
+
 
 class Circle:
-    def __init__(self, radius: float):
-        self.params = {"radius": radius}
+    def __init__(self, **params):
+        self.params = params
+        self.radius = params["radius"]
 
     def area(self) -> float:
-        return self.params["radius"] ** 2 * pi
+        return math.pi * self.radius ** 2
 
 
-class Rectangle:
-    def __init__(self, width: float, height: float):
-        self.width = {"width": width}
-        self.height = {"height": height}
+class Triangle:
+    """
+    Represents a geometric Triangle with methods to validate its properties
+    and calculate its area.
+
+    :param sides: Lengths of the three sides of the triangle. Must be positive numbers.
+    :type sides: Union[int, float]
+    :raises ValueError: If the number of sides is not equal to 3.
+    :raises ValueError: If any side length is non-positive.
+    :raises ValueError: If the sum of any two sides is not greater than the third side.
+    """
+    def __init__(self, *sides: Union[int, float]) -> None:
+        """
+        :param sides: Lengths of the three sides of the triangle. Must be positive numbers.
+        :type sides: Union[int, float]
+        :raises ValueError: If the number of sides is not 3.
+        :raises ValueError: If any side is non-positive.
+        :raises ValueError: If the sum of two sides is not greater than the third.
+        """
+        if len(sides) != 3:
+            raise ValueError("Triangle must have 3 sides.")
+        if any(side <= 0 for side in sides):
+            raise ValueError("Triangle sides must be positive.")
+        self.a, self.b, self.c = sorted(sides)
+        if self.a + self.b <= self.c:
+            raise ValueError("Triangle is not valid: sum of two sides must be greater than third.")
+        self.p = (self.a + self.b + self.c) / 2  # half perimeter of sides
+
+    def _is_right(self) -> bool:
+        """
+        Check if the triangle is right-angled.
+
+        :returns: True if the triangle is right-angled, False otherwise.
+        :rtype: bool
+        """
+        return math.isclose(self.a ** 2 + self.b ** 2, self.c ** 2)
 
     def area(self) -> float:
-        return self.width["width"] * self.height["height"]
+        """
+        Calculates the area of the triangle.
+
+        If the triangle is right-angled, the area is calculated as
+        (base * height) / 2.
+
+        If the triangle is not right-angled, the area is calculated
+        using Heron's formula.
+
+        :returns: The area of the triangle.
+        :rtype: float
+        """
+        if self._is_right():
+            return (self.a * self.b) / 2
+        return math.sqrt(self.p * (self.p - self.a) * (self.p - self.b) * (self.p - self.c))
--- src/protocols.py
+++ src/protocols.py
@@ -2,8 +2,7 @@ from typing import Protocol
 
 
 class Figure(Protocol):
-    name: str
-    params: dict[str, float]
+
     def area(self) -> float:
         pass
 
